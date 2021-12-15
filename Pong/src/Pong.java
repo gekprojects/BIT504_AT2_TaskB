6242faffab46e102b67c04e818d939f974c51b2a
gekprojects <gemmakrebs@live.com>
2021-12-15 16:50:29 +1300

    Second attempt at updating GitHub

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -3,7 +3,7 @@ public class Pong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Pong game");
+		System.out.println("This is the Pong game");
 	}
 
 }
