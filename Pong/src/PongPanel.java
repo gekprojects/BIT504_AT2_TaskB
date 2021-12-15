b5330c4aebc34f7d033304e173aa32c7830da739
gekprojects <gemmakrebs@live.com>
2021-12-16 09:39:43 +1300

    Added paint and timer to project

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -1,3 +1,5 @@
+//import javax.management.timer.Timer;
+import javax.swing.Timer;
 import javax.swing.JPanel;
 
 import java.awt.event.ActionEvent;
@@ -5,14 +7,30 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	
 	private final static Color BACKGROUND_COLOR = Color.BLACK;
+	private final static int TIMER_DELAY = 5;
 	
 	public PongPanel() {
 		setBackground(BACKGROUND_COLOR);
+		Timer timer = new Timer(TIMER_DELAY, this);
+		timer.start();
+	}
+	
+	private void update() {
+		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);;
+		g.setColor(BACKGROUND_COLOR.WHITE);
+		g.fillRect(20, 20, 100, 100);
 	}
 
 	@Override
@@ -36,6 +54,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		// TODO Auto-generated method stub
+		update();
 		
 	}
 
